21aa01289c94c9ca8a2e32d81966fcdef9f35a0a
Amrish Kumar G <amrishkumarg@virtusa.com>
2022-03-15 01:59:07 +0530

    json file reader added

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import ordersdata from './orderlist.json';
+interface Orders{
+  product_name: String;
+  price: number;
+  quantity: number;
+}
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.css']
 })
+
 export class OrdersComponent implements OnInit {
 
   constructor() { }
-
+  orderlists:Orders[]=ordersdata;
   ngOnInit(): void {
   }
 
